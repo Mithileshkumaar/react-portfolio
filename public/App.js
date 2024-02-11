029de458ada298743339793c09f0f23711c65994
Mithileshkumaar <mithilesh.kumaar@yahoo.com>
2024-02-11 18:29:30 +0530

    updated

--- public/App.js
+++ public/App.js
@@ -1,29 +1,29 @@
 
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <nav className="navbar navbar-light App p-5">
-      <div className="container-fluid p-2">
-        <h3 className="navbar-brand">.MITHILESH KUMAAR</h3>
+// function App() {
+//   return (
+//     <nav className="navbar navbar-light App p-5">
+//       <div className="container-fluid p-2">
+//         <h3 className="navbar-brand">.MITHILESH KUMAAR</h3>
 
-        <form className="d-flex m-10">
-
-
-          <button className='button  '>
-            HOME  </button>
-          <button className='button '>
-            PROJECTS  </button>
-          <button className='button '>
-            ABOUT ME  </button>
-          <button className='button '>
-            CONTACT  </button>
+//         <form className="d-flex m-10">
 
 
-        </form>
-      </div>
-    </nav>
-  );
-}
+//           <button className='button  '>
+//             HOME  </button>
+//           <button className='button '>
+//             PROJECTS  </button>
+//           <button className='button '>
+//             ABOUT ME  </button>
+//           <button className='button '>
+//             CONTACT  </button>
 
-export default App;
+
+//         </form>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default App;
